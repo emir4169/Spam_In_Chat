19b6771deb4408f0ea574f17c3f5bf64ce11c94a
Winwin <em.ireri41@gmail.com>
2023-06-11 09:38:22 +0300

    hell yea mixing together the gradle crap of both mods
    hell yea mixing together the gradle crap of both mods

--- src/main/java/spam/in/chat/ExampleMod.java
+++ src/main/java/spam/in/chat/ExampleMod.java
@@ -1,22 +1,19 @@
 package spam.in.chat;
 
 import net.fabricmc.api.ModInitializer;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ExampleMod implements ModInitializer {
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("spam-in-chat");
 
+	// an instance of our new item
+	public static final Item CUSTOM_ITEM =
+			Registry.register(Registries.ITEM, new Identifier("spam", "spam"),
+					new Item(new FabricItemSettings().maxCount(127)));
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
 	}
 }
